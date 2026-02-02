8603fabbdc89fec0d64503c03dc051f398d499de
anandakrishnan tm <146854207+anandhu62@users.noreply.github.com>
2026-02-02 22:09:45 +0530

    inLoop.c

---
+++ inLoop.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+ int i,j;
+ clrscr();
+
+ for(i=1;i<=4;i++)
+ {
+  for(j=i;j<=4;j++)
+  {
+   printf("%d ",j);
+  }
+  printf("\n");
+ }
+ getch();
+ return 0;
+}
