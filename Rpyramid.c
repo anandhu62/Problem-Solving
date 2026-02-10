4ceaa7ed12ea100b9ff448fbf3955b18cac7279b
anandakrishnan tm <146854207+anandhu62@users.noreply.github.com>
2026-02-10 16:31:18 +0530

    Rpyramid.c

---
+++ Rpyramid.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+ int i,j,n;
+ printf("Enter the size of the pyramid:");
+ scanf("%d",&n);
+
+ for(i=1;i<=n;i++)
+ {
+  for(j=1;j<=i;j++)
+  {
+   printf("*");
+  }
+  printf("\n");
+ }
+ getch();
+ return 0;
+}
